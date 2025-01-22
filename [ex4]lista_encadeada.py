766ca594ccf0747fa6f25cc2a0b5b84e7a40a8f5
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2025-01-22 10:11:06 -0300

    new "separa lista exercice" and add comparacoes.py functions in [ex4]lista_encadeada class

--- [ex4]lista_encadeada.py
+++ [ex4]lista_encadeada.py
@@ -81,8 +81,10 @@ class lista_encadeada:
             for i in range(num):
                 anterior = cursor
                 cursor = cursor.prox
+            # elemento = cursor.carga
             anterior.prox = cursor.prox
         self.__tamanho -= 1
+        # return elemento
 
     def modifica(self, num, carga):
         if num < 0 or num >= self.__tamanho:
@@ -122,6 +124,62 @@ class lista_encadeada:
             cont += 1
         return -1
 
+    def uniao(self, lista1, lista2):
+        lista_n = lista_encadeada()
+        
+        for i in range(len(lista1)):
+            lista_n.insere_final(lista1.busca_elemento(i))
+        
+        for i in range(len(lista2)):
+            lista_n.insere_final(lista2.busca_elemento(i))
+        
+        return lista_n
+
+    def intersecao(self, lista1, lista2):
+        lista_n = lista_encadeada()
+        for i in range(len(lista1)):
+            try:
+                lista_n.insere_final( lista2.busca( lista1.busca_elemento(i) ) )
+            except ListaError:
+                pass
+        return lista_n
+
+    def diferenca(self, lista1, lista2):
+        lista_n = lista_encadeada()
+        for i in range(len(lista1)):
+            try:  
+                lista2.busca( lista1.busca_elemento(i) )
+            except ListaError:
+                lista_n.insere_final(lista1.busca_elemento(i))
+        
+        return lista_n
+    
+    def separa(self, n):
+        if self.vazia():
+            raise ListaError("a lista está vazia")
+        if n < 0 or n >= self.__tamanho:
+            raise ListaError("valor fora do intervalo")
+
+        lista_n = lista_encadeada()
+        cursor = self.__head
+        cont = 0
+        anterior = None
+
+        # Percorre a lista até o índice n
+        while cursor != None and cont < n:
+            anterior = cursor
+            cursor = cursor.prox
+            cont += 1
+
+        # Se `cursor` está no índice `n`, ele será o novo início da lista separada
+        if cursor != None:
+            lista_n.__head = cursor  # Nova lista começa em cursor
+            lista_n.__tamanho = self.__tamanho - n
+            anterior.prox = None  # Quebra a referência da lista original
+            self.__tamanho = n
+
+        return lista_n
+
 #teste
 if __name__ == '__main__':
     #teste lista sequencial
@@ -133,11 +191,40 @@ if __name__ == '__main__':
     teste.insere_final(50)
     print(teste)
     print('-------')
-    teste.insere_ordenado(1)
-    teste.insere_ordenado(35)
-    teste.insere_ordenado(60)
+    # teste.insere_ordenado(1)
+    # teste.insere_ordenado(35)
+    # teste.insere_ordenado(60)
+    # print(teste)
+    print('-------')
+
+    print(teste.separa(1))
     print(teste)
     print('-------')
+
+    l1 = [1, 2, 3, 4, 5, 10, 20]
+    l2 = [1, 9, 3, 8, 5]
+
+    lista1 = lista_encadeada()
+    lista2 = lista_encadeada()
+    for i in l1:
+        lista1.insere_final(i)
+
+    for i in l2:
+        lista2.insere_final(i)
+
+    print('listas:')
+    print(lista1)
+    print(lista2)
+
+    print('união das listas:')
+    print(teste.uniao(lista1, lista2))
+
+    print('interseção das listas:')
+    print(teste.intersecao(lista1, lista2))
+
+    print('diferença das listas:')
+    print(f'diferença da lista1 para a lista2: {teste.diferenca(lista1, lista2)}')
+    print(f'diferença da lista2 para a lista1: {teste.diferenca(lista2, lista1)}')
     
     # teste.insere(1, '0.5')
     # teste.insere(3, '3.5')
